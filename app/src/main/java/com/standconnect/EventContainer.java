30c67b5b097c8688d9d4d22290da7d17e17b0522
apple123 <j.mc0207@gmail.com>
2015-11-23 19:08:23 +0100

    Se crearn los fragments DetailView, ProductDetail y Profile

--- app/src/main/java/com/standconnect/EventContainer.java
+++ app/src/main/java/com/standconnect/EventContainer.java
@@ -2,20 +2,19 @@ package com.standconnect;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -148,7 +147,7 @@ public class EventContainer extends AppCompatActivity
 
         } else if (id == R.id.nav_profile) {
             //FormUserProfile
-            fragment = new ListEventContainerFragment();
+            fragment = new ProfileFragment();
             dataContentList = eventContainerController.getAllBusiness("eventid");
 
         }else{
